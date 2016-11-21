4804669905f1292c53bde16ef174616e2366d4a7
wangtao725 <wangtaody1987@gmail.com>
2016-11-21 17:12:32 +0800

    添加新文件

--- DBEN.ETM/DBIWebApp.cs
+++ DBEN.ETM/DBIWebApp.cs
@@ -1,72 +1,61 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using DBEN.VATEngine;
-//using DBEN.WF;
-//using Rafy;
-//using Rafy.Accounts;
-//using Rafy.Data;
-//using Rafy.Domain.EntityPhantom;
-//using Rafy.Domain.Stamp;
-//using Rafy.RBAC;
-//using Rafy.Web;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Rafy;
+using Rafy.Data;
+using Rafy.RBAC;
+using Rafy.Web;
+using DBEN.ETM.Common.Descript;
 
-//namespace DBEN.ETM.Web
-//{
-//    class ETMWebApp : WebApp
-//    {
-//        protected override void InitEnvironment()
-//        {
-//            RafyEnvironment.DomainPlugins.Add(new StampPlugin());
-//            RafyEnvironment.DomainPlugins.Add(new EntityPhantomPlugin());
-//            RafyEnvironment.DomainPlugins.Add(new AccountsPlugin());
-//            RafyEnvironment.DomainPlugins.Add(new RBACPlugin());
-//            RafyEnvironment.DomainPlugins.Add(new WFPlugin());
-//            RafyEnvironment.DomainPlugins.Add(new DBIPlugin());
-//            RafyEnvironment.DomainPlugins.Add(new VATEnginePlugin());
+namespace DBEN.ETM.Web
+{
+    class ETMWebApp : WebApp
+    {
+        protected override void InitEnvironment()
+        {
 
-//            this.AddCustomiedProject();
+            RafyEnvironment.DomainPlugins.Add(new RBACPlugin());
 
-//            AccountsPlugin.DbSettingName =
-//                WFPlugin.DbSettingName =
-//                RBACPlugin.DbSettingName =
-//                VATEnginePlugin.DbSettingName;
+            this.AddCustomiedProject();
 
-//            this.DecryptConnectionString();
 
-//            base.InitEnvironment();
-//        }
+            RBACPlugin.DbSettingName = "ETM";
 
-//        protected virtual void AddCustomiedProject() { }
+            this.DecryptConnectionString();
 
-//        /// <summary>
-//        /// 如果配置文件的给connection加密属性设置为ture，那么程序运行会走这个方法
-//        /// 这个方法通过AES加解密来保护connection不被发现，默认的key是DBEN
-//        /// </summary>
-//        protected virtual void DecryptConnectionString()
-//        {
-//            if (ConfigurationHelper.GetAppSettingOrDefault("DBEN.DBI.Data.UseEncryptedConnectionString", false))
-//            {
-//                var settingName = VATEnginePlugin.DbSettingName;
-//                var settings = DbSetting.FindOrCreate(settingName);
-//                var encriptionStr = settings.ConnectionString;//获取connection
-//                var decriptionStr = Common.DescriptAES.AESDecrypt(encriptionStr, "DBEN");//解密后的connection
-//                DbSetting.SetSetting(settingName, decriptionStr, settings.ProviderName);
-//            }
-//        }
+            base.InitEnvironment();
+        }
 
-//        protected override void OnRuntimeStarting()
-//        {
-//            base.OnRuntimeStarting();
+        protected virtual void AddCustomiedProject() { }
 
-//            DbAutoUpdater.Update();
-//        }
-//        protected override void OnStartupCompleted()
-//        {
-//            base.OnStartupCompleted();
-//            RafyEntityJScriptHandler rafyEntityScript = new RafyEntityJScriptHandler();
-//            rafyEntityScript.GenerateScript();
-//        }
-//    }
-//}
+        /// <summary>
+        /// 如果配置文件的给connection加密属性设置为ture，那么程序运行会走这个方法
+        /// 这个方法通过AES加解密来保护connection不被发现，默认的key是DBEN
+        /// </summary>
+        protected virtual void DecryptConnectionString()
+        {
+            if (ConfigurationHelper.GetAppSettingOrDefault("DBEN.DBI.Data.UseEncryptedConnectionString", false))
+            {
+                var settingName = RBACPlugin.DbSettingName;// VATEnginePlugin.DbSettingName;
+                var settings = DbSetting.FindOrCreate(settingName);
+                var encriptionStr = settings.ConnectionString;//获取connection
+                var decriptionStr = DescriptAES.AESDecrypt(encriptionStr, "DBEN");//解密后的connection
+                DbSetting.SetSetting(settingName, decriptionStr, settings.ProviderName);
+            }
+        }
+
+        //protected override void OnRuntimeStarting()
+        //{
+        //    base.OnRuntimeStarting();
+
+        //    DbAutoUpdater.Update();
+        //}
+        //protected override void OnStartupCompleted()
+        //{
+        //    base.OnStartupCompleted();
+        //    RafyEntityJScriptHandler rafyEntityScript = new RafyEntityJScriptHandler();
+        //    rafyEntityScript.GenerateScript();
+        //}
+    }
+}
